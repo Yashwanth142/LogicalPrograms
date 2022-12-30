b1b97f99a6bb106809a7f37aa52ea4a43c2918cc
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-30 17:08:23 +0530

    Prime Number added

---
+++ PrimeNum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNum
+    {
+        public static void Primenum() 
+        {
+            Console.WriteLine("Enter a number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int p=0;
+            for (int i=2 ; i<=num/2 ; i++)
+            {
+                if(num%i==0)
+                {
+                    Console.WriteLine("The Enter number is not Prime number");
+                    p = 1;
+                    break;
+                }
+            }
+            if (p == 0)
+            {
+                Console.WriteLine("The Enter number is Prime number");
+            }
+            
+        }
+    }
+}
