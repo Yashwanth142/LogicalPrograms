bb6328437e22cf5f32314bcfedf2725d6456b3c1
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-30 17:02:19 +0530

    Logical programs added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LogicalPrograms
+{
+    public class Programs
+    {
+        public static void Main(string[] args)
+        {
+            FibonacciSeries.Fibonacci();
+
+            PerfectNum.CheckPerfectNum();
+
+            PrimeNum.Primenum();
+
+            ReverseNum.Reverse();
+
+            CouponNumber.CouponNum();
+
+            StopWatch.Check();
+
+        }
+    }
+}
