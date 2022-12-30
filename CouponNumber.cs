8de1c688512ba29eb431a7af9127f666fb5a3836
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-30 17:06:14 +0530

    Coupon Number added

---
+++ CouponNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class CouponNumber
+    {
+        public static void CouponNum()
+        {
+           int distinct = 0,count=0;
+            Console.WriteLine("Enter the number of distnict coupons");
+            int couponnum=Convert.ToInt32(Console.ReadLine());
+
+            bool[] isCollect = new bool[couponnum];
+
+            while(distinct<couponnum)
+            {
+                Random random= new Random();
+                int newCoupon = (int)random.Next(couponnum);
+
+                count++;
+
+                if (!isCollect[newCoupon])
+                {
+                    distinct++;
+                    isCollect[newCoupon] = true;
+                }
+            }
+            Console.WriteLine("Total random number needed to have all distinct coupons : " + count);
+        }
+    }
+}
